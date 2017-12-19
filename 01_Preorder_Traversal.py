04b75406a089cc8faac815daa47b43d9fe918dad
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-18 23:35:30 -0600

    Create 01_Preorder_Traversal.py

---
+++ 01_Preorder_Traversal.py
@@ -0,0 +1,14 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if(root == None):
+        return
+    print root.data,
+    preOrder(root.left)
+    preOrder(root.right)
+    
